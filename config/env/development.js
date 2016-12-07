af5e3ec7823d82d74760c20388cb297ad4769163
Henrique Alexandre <alexandressh@gmail.com>
2016-12-07 21:14:25 +0000

    First commit

---
+++ config/env/development.js
@@ -0,0 +1,8 @@
+var port = process.env.PORT;
+var ip   = process.env.IP;
+
+module.exports = {
+  // Development configuration options
+  db: 'mongodb://alexandressh-mean-3940194:27017/mean-book',
+  sessionSecret: 'developmentSessionSecret'
+};
